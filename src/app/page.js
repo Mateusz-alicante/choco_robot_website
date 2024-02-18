5ea38769427ffceee49f73096ff83145902006ef
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2024-02-18 10:47:51 -0500

    added color buttons

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,15 @@ export default function Home() {
         <div className={styles.innerContainer}>
           <h1 className={styles.title}>Love is in the air!</h1>
           <ShowImage />
+
+          <div className={styles.colorSelection}>
+            <h2 className={styles.title}>Favorite color</h2>
+            <div className={styles.colorButtons}>
+              <button className={styles.redButton}>Red</button>
+              <button className={styles.greenButton}>Green</button>
+              <button className={styles.blueButton}>Blue</button>
+            </div>
+          </div>
         </div>
       </div>
     </main>
